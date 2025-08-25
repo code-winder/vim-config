3cba34c6cf1f520dfaf03dc16dc054ceebb4c371
code-winder <118537583+code-winder@users.noreply.github.com>
2025-08-25 16:13:10 +0800

    Update disable-plugin.lua

--- config/nvim/plugins/disable-plugin.lua
+++ config/nvim/plugins/disable-plugin.lua
@@ -3,6 +3,24 @@ return {
     "echasnovski/mini.ai",
     --- 禁用 mini.ai 插件
     enabled = false,
-    opt = {},
+  },
+  {
+    "SirVer/ultisnips",
+    --- 与 coc.nvim 配合使用
+    enabled = false,
+  },
+  {
+    "honza/vim-snippets",
+    --- 与 coc.nvim 配合使用
+    enabled = false,
+  },
+  {
+    "charlespascoe/vim-go-syntax",
+    --- 与 coc.nvim 配合使用
+    enabled = false,
+  },
+  {
+    "junegunn/vim-peekaboo",
+    enabled = false,
   },
 }
