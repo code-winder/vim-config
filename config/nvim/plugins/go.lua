ac23179821e6c70aa2b06c771df00616dafb53d4
code-winder <118537583+code-winder@users.noreply.github.com>
2025-11-21 21:35:14 +0800

    Update go.lua

--- config/nvim/plugins/go.lua
+++ config/nvim/plugins/go.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "buoto/gotests-vim",
-    --- 模版一
-    --- 该 templates 来自git仓库 dotLou/goteststemplates
-    --- 用 testtify 的 require 包，某个test函数失败就立马停止
-    --- let g:gotests_template_dir = '$HOME/.vim/go-tests-template/goteststemplates/templates'
-    --- 模版二
-    --- 该 templates 来自git仓库 ras0q/gotests-template
-    --- 用 https://github.com/google/go-cmp 包做断言
-    --- 需要在 templates2 的header.tmpl 把 cmp 的包路径添加进去
-    --- Call :GoTests to generate a test for the function at the current line or functions selected in visual mode.
-    --- Call :GoTestsAll to generate tests for all functions in the current buffer.
-    opt = {},
-    event = "VeryLazy",
-    config = function(_, opts)
-      vim.cmd("let g:gotests_template_dir = $HOME . '/.vim/gotests-template/templates2'")
-    end,
-    cmd = { "GoTests", "GoTestsAll" },
-  },
+  -- {
+  --   "buoto/gotests-vim",
+  --   --- 模版一
+  --   --- 该 templates 来自git仓库 dotLou/goteststemplates
+  --   --- 用 testtify 的 require 包，某个test函数失败就立马停止
+  --   --- let g:gotests_template_dir = '$HOME/.vim/go-tests-template/goteststemplates/templates'
+  --   --- 模版二
+  --   --- 该 templates 来自git仓库 ras0q/gotests-template
+  --   --- 用 https://github.com/google/go-cmp 包做断言
+  --   --- 需要在 templates2 的header.tmpl 把 cmp 的包路径添加进去
+  --   --- Call :GoTests to generate a test for the function at the current line or functions selected in visual mode.
+  --   --- Call :GoTestsAll to generate tests for all functions in the current buffer.
+  --   opt = {},
+  --   event = "VeryLazy",
+  --   config = function(_, opts)
+  --     vim.cmd("let g:gotests_template_dir = $HOME . '/.vim/gotests-template/templates2'")
+  --   end,
+  --   cmd = { "GoTests", "GoTestsAll" },
+  -- },
   {
     "charlespascoe/vim-go-syntax",
     --- 加强版 go 语法高亮
